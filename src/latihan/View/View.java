47fb3d5f765d22040aa4ae5273500793f474c330
Harisatul <haris.aulia404@gmail.com>
2021-10-24 21:39:00 +0700

    test

--- src/latihan/View/View.java
+++ src/latihan/View/View.java
@@ -15,28 +15,52 @@ public class View {
     }
 
     public void showLibary(){
-        System.out.println("\nDAFTAR BUKU");
-        service.show();
+        label:
+        while (true) {
+            System.out.println("\nDAFTAR MAHASISWA");
+            service.show();
 
-        System.out.println("Masukkan Data");
+            System.out.println("== MENU");
+            System.out.println("1. Tambah\n2. Hapus\n3. Cari\n4. Ubah\nx. Keluar");
+            String input = s.nextLine();
 
-        System.out.println("Masukkan judul");
+            switch (input) {
+                case "1":
+                    addLibary();
+                    break;
+                case "2":
+//                    removeMahasiswa();
+                    break;
+                case "3":
+//                    searchMahasiswa();
+                    break;
+                case "4":
+//                    updateMahasiswa();
+                    break;
+                case "x":
+                    break label;
+                default:
+                    System.out.println("Pilihan tidak dimengerti");
+                    break;
+            }
+        }
+        System.out.println("\nTerima kasih ~");
+
+    }
+
+    public  void addLibary(){
+        System.out.print("Masukkan judul buku : ");
         String name = s.nextLine();
 
-        System.out.println("Masukkan publisher");
+        System.out.print("Masukkan publisher : ");
         String publisher = s.nextLine();
 
-        System.out.println("Masukkan author");
+        System.out.print("Masukkan author : ");
         String author = s.nextLine();
 
-        System.out.println("Masukkan tahun rilis");
+        System.out.print("Masukkan tahun rilis : " );
         int year = s.nextInt();
-
         service.addLibrary(name,publisher,author,year);
-
-
-        service.show();
-
 
 
 
